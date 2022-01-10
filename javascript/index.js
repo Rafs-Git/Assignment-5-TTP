78cc65eec14f80456130b7a108e4a6c94e8ad889
Max Yedid <maxyedid1@gmail.com>
2022-01-10 12:17:50 -0500

    Fixing merge issue

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,1 @@
-let rowCount = 1;
-let columnCount = 1;
 
-document.getElementById("remove-column").addEventListener("click", () => {
-    if (columnCount < 2) {
-        return;
-    }
-    const allRows = document.querySelectorAll("tr");
-    for (let i = 0; i < allRows.length; i++) {
-        allRows[i].lastChild.remove();
-    }
-    columnCount--
-})
