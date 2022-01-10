232c991ddfcabbce09880fd4c04f53603bbff888
Max Yedid <maxyedid1@gmail.com>
2022-01-10 14:35:19 -0500

    Added the logic for both changing colors from dropdown menu and also for changing the color of a certain cell

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,17 @@
-
 let rowCount = 1;
 let columnCount = 1;
+let currentColor = "white"
 let grid = document.getElementById("main-table")
+const firstRow = document.createElement("tr")
+const firstCell = document.createElement("td")
+const firstButton = document.createElement("button")
+firstButton.className = "color-grid"
+    firstButton.addEventListener("click", () => {
+        this.style.backgroundColor = currentColor
+    })
+firstCell.append(firstButton)
+firstRow.append(firstCell)
+grid.append(firstRow)
 
 //This function does the logic for adding a row into the grid
 document.getElementById("add-row").addEventListener("click", () => {
@@ -10,6 +20,9 @@ document.getElementById("add-row").addEventListener("click", () => {
     for (let i = 0; i < columnCount; i++) {
         let newCell = document.createElement("td");
         newCell.innerHTML = '<button class = "color-grid"></button>'
+        newCell.addEventListener("click", () => {
+            newCell.style.backgroundColor = currentColor
+        })
         newRow.append(newCell)
     }
     document.getElementById("main-table").append(newRow)
@@ -22,6 +35,9 @@ document.getElementById("add-column").addEventListener("click", () => {
     for (let i = 0; i < rows.length; i++) {
         let newCell = document.createElement("td");
         newCell.innerHTML = '<button class = "color-grid"></button>'
+        newCell.addEventListener("click", () => {
+            newCell.style.backgroundColor = currentColor
+        })
         rows[i].append(newCell)
     }
 })
@@ -54,14 +70,45 @@ document.getElementById("reset").addEventListener("click", () => {
     if (rowCount < 2 && columnCount < 2) {
         return;
     }
+    currentColor = "white"
     while (grid.hasChildNodes()) {
         grid.lastChild.remove();
     }
     let newRow = document.createElement("tr")
     let newCell = document.createElement("td")
     newCell.innerHTML = '<button class = "color-grid"></button>'
+    newCell.addEventListener("click", () => {
+        newCell.style.backgroundColor = currentColor
+    })
     newRow.append(newCell)
     grid.append(newRow)
     rowCount = 1;
     columnCount = 1;
+})
+
+//All the buttons from the dropdown menu
+//Added logic to change currentcolor to the selected one
+document.getElementById("blue").addEventListener("click", () => {
+    currentColor = "blue"
+})
+document.getElementById("red").addEventListener("click", () => {
+    currentColor = "red"
+})
+document.getElementById("green").addEventListener("click", () => {
+    currentColor = "green"
+})
+document.getElementById("pink").addEventListener("click", () => {
+    currentColor = "pink"
+})
+document.getElementById("orange").addEventListener("click", () => {
+    currentColor = "orange"
+})
+document.getElementById("white").addEventListener("click", () => {
+    currentColor = "white"
+})
+document.getElementById("yellow").addEventListener("click", () => {
+    currentColor = "yellow"
+})
+document.getElementById("black").addEventListener("click", () => {
+    currentColor = "black"
 })
